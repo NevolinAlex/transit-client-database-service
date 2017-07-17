ba9373688c5fdd6286fcf1c40de0a7383ab58ba7
Александр Неволин <disaplase@yandex.ru>
2017-07-17 23:12:48 +0500

    initial commit

---
+++ src/main/java/ru/itclick/TransitDatabaseServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.itclick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitDatabaseServiceApplication.class, args);
+	}
+}
